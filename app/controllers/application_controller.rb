41b6d7c55ae796d723f1fa44d02ecd4c8cc23982
bransynluther <bransynluther@gmail.com>
2017-11-29 19:24:58 -0500

    create inc and dec methods, create session method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :sessionHandler
+  def sessionHandler
+   if session['session_id']
+    begin
+     @session_id = ItemTotal.find(session['session_id'])
+
+    rescue
+     @session_id = ItemTotal.create
+     session['session_id'] = @session_id.id
+    end
+
+    else
+      @session_id = ItemTotal.create
+      session['session_id'] = @session_id.id
+   end
+  end
 end
